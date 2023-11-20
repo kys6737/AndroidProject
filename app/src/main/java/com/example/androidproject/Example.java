20e39843a366731039a2ad4fd406d5b914e56576
PSYUN <park48569803@gmail.com>
2023-11-20 18:35:32 +0900

    구조 완성

---
+++ app/src/main/java/com/example/androidproject/Example.java
@@ -0,0 +1,12 @@
+package com.example.androidproject;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Example extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.example);
+    }
+}
--- app/src/main/java/com/example/androidproject/MainActivity.java
+++ app/src/main/java/com/example/androidproject/MainActivity.java
@@ -1,14 +1,130 @@
 package com.example.androidproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView message, name, date_FB, time_FB;
+    Button alert, myPage, consul, iljeongCheck, history, ilJeong, myProfessor;
+
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        name = findViewById(R.id.Name);
+        date_FB = findViewById(R.id.Date_FB);
+        time_FB = findViewById(R.id.Time_FB);
+        message = findViewById(R.id.Message);
+        alert = findViewById(R.id.alert);
+        consul = findViewById(R.id.Consultation);
+        iljeongCheck = findViewById(R.id.IljeongButton);
+        history = findViewById(R.id.History);
+        ilJeong = findViewById(R.id.Iljeong);
+        myProfessor = findViewById(R.id.MyProfessor);
+        myPage = findViewById(R.id.MyPage);
+
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference("example/ex1");
+
+        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.exists()){
+                    String firebaseName = snapshot.child("name").getValue(String.class);
+                    String firebaseDate = snapshot.child("date").getValue(String.class);
+                    String firebaseTime = snapshot.child("time").getValue(String.class);
+
+                    name.setText(firebaseName);
+                    date_FB.setText(firebaseDate);
+                    time_FB.setText(firebaseTime);
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        alert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Example.class);
+                startActivity(intent);
+
+            }
+        });
+
+        consul.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Example.class);
+                startActivity(intent);
+
+            }
+        });
+
+        iljeongCheck.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Example.class);
+                startActivity(intent);
+
+            }
+        });
+
+        history.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Example.class);
+                startActivity(intent);
+
+            }
+        });
+
+        ilJeong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Example.class);
+                startActivity(intent);
+
+            }
+        });
+
+        myProfessor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Example.class);
+                startActivity(intent);
+
+            }
+        });
+
+        myPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Example.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
